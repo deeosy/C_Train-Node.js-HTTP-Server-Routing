7a646153f7a1fbe236c1886140470aa6c8bccc43
Derrode Walter Cheale <144121696+deeosy@users.noreply.github.com>
2025-02-24 10:41:25 +0000

    added services and time route

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const http= require("http");
+// const fs = require('fs');
+// const path = require('path');
 
 const handleAllRequest = (req, res) => {
   const url = req.url;
+  const time = new Date().toLocaleTimeString()
+  
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-  if(url === "/"){
+  if(url === "/"|| url === "/home"){
     res.write("<h1>Welcome to my Node.js Server</h1>")
+    // // how to read file index.html
+    // fs.readFile(path.join())
+
   } else if (url === '/about'){
     res.write("<h1>This is the About page</h1>")
   } else if (url === '/contact'){
     res.write("<h1>Contact us at: deeosy2@gmail.com </h1>")
-  }else{
+  }else if (url === '/services'){
+    res.write("<h1>We offer Web Development and Mobile App Development services</h1>")
+  }else if (url === '/time'){
+    res.write(`<h1>Current time: ${time} </h1>`)
+  } else{
     res.write("<h1> 404 Page Not Found </h1>")
   }
 
